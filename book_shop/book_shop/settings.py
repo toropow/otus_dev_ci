c2bca0282cd0b44800cd075f119cfb2169f04bf3
Anton <toropow@gmail.com>
2020-12-17 19:21:45 +0300

    remove env pg host

--- book_shop/book_shop/settings.py
+++ book_shop/book_shop/settings.py
@@ -13,8 +13,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str('SECRET_KEY')
 
 
-ALLOWED_HOSTS = []
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
